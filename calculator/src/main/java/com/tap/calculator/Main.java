2b323791d2f37dd0d60fa542aae6b0189fce7a5f
merienxx <eugenioanna389@gmail.com>
2023-03-06 11:51:46 +0800

    Moved classes and packages to calculator-gateway project

--- calculator/src/main/java/com/tap/calculator/Main.java
+++ calculator/src/main/java/com/tap/calculator/Main.java
@@ -1,11 +1,13 @@
 package com.tap.calculator;
- 
-import com.tap.calculator.events.ButtonClick;
+
+import com.tap.calculator.beans.CalculatorFrame;
 
 public class Main {
-	@SuppressWarnings("unused")
-	private ButtonClick btnClick = new ButtonClick();
-	public Main() {}
+	
+	public Main() {
+		CalculatorFrame main = new CalculatorFrame();
+		main.display();
+	}
 	
 	public static void main(String[] args) {
 		new Main();
